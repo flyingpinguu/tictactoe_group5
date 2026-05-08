f325ea5549af8aadcc3ab26b8749620353ddc88b
flyingpinguu <alexander.christinck@gmail.com>
2026-05-08 16:14:20 +0200

    our game almost finished

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -4,20 +4,60 @@
 # If you run `python tic_tac_toe.py` in the command line the game will start. Try it out! ;)
 
 # Function for ... (displaying the board?)
-def blabla():
-    pass
+grid=[[" ", " ", " "],[" ", " ", " "],[" ", " ", " "]]
+print("Welcome to a new round of Tic-Tac-Toe!")
+turn = 1
+def check_space(line,column):
+    if grid[line][column] == " ":
+        return True
+    else:
+        return False
+def printboard():
+    for line in grid:
+        print(line)
+def userinput(symbol):
+    line = int(input("Write the number of the line you want to place your symbol: "))
+    column = int(input("Write the number of the column you want to place: "))
+    line -= 1
+    column -= 1
+    if check_space(line,column) == False:
+        return False
+    grid[line][column]=symbol
+    return True
+def checkwin():
+
+    if grid[0][0] == grid[1][1] == grid[2][2] and grid[0][0] != " ": #diagonal 1
+        return True
+    if grid[0][2] == grid[1][1] == grid[2][0] and grid[1][1] != " ": # check for diagnol 2
+        return True
+    for i in range(len(grid)):
+        if grid[i][0] == grid[i][1] == grid[i][2] and grid[i][0] != " ": # check rows
+            return True
+    for j in range(len(grid)):
+        if grid[0][j] == grid[1][j] == grid[2][j] and grid[0][j] != " ": # check columns
+            return True
+
+while True:
+    players = ["x", "o"]
+    active_symbol = players[turn%2]
+    if userinput(active_symbol) == False:
+        print("That didn't work enter new space")
+        continue
+    if checkwin() == True:
+        print("Congrats you won!")
+        printboard()
+        break
+    printboard()
+    if turn >= 9:
+        print("Tie!")
+        break
+    turn += 1
 
 
-# Function for... (choosing a player?)
-def blablabla():
-    pass
-
-
-# ... write as many functions as you need
 
 
 # Tic-tac-toe game
 if __name__ == "__main__":
 
     # Start a new round of Tic-tac-toe
-    print("Welcome to a new round of Tic-Tac-Toe!")
+    print("this is the end")
